47d1fe58980a0b873e9d84c6c3909673dfaadb2a
Hugo <you@example.com>
2024-05-11 21:36:52 +0200

    refactor: same behavior as hw2, but extracted all functions to crate

--- homework3/src/main.rs
+++ homework3/src/main.rs
@@ -1,11 +1,48 @@
-fn incr(n: &mut i32) {
-    *n += 1;
-}
+mod stringlib;
+
+use std::env;
+// use std::error::Error;
+use std::io::stdin;
+
+// fn to_snakecase(input_string: String) -> String {
+//     return input_string.trim().replace(' ', "-").to_lowercase();
+// }
 
 fn main() {
-    let mut n = 1;
+    let args: Vec<String> = env::args().collect();
+    if !stringlib::are_arguments_valid(&args) {
+        std::process::exit(1);
+    }
 
-    incr(&mut n);
+    println!();
 
-    println!("{n}");
+    let user_option = &args[1];
+
+    println!("You chose option: {user_option}");
+    println!();
+
+    println!("Please enter line to transform");
+    let mut input_string = String::new();
+    stdin()
+        .read_line(&mut input_string)
+        .expect("Failed to read user text from stdin");
+
+    println!();
+    println!("Input string:");
+    println!("{input_string}");
+
+    let output_string = match user_option.as_str() {
+        "--lowercase" => stringlib::to_lowercase(input_string),
+        "--uppercase" => stringlib::to_uppercase(input_string),
+        "--no-spaces" => stringlib::to_no_spaces(input_string),
+        "--snake-case" => stringlib::to_snakecase(input_string),
+        "--slugify" => stringlib::to_slugified(input_string),
+        _ => {
+            unreachable!("Unknown argumetns should have been handled before.");
+        }
+    };
+
+    println!();
+    println!("Resulting string:");
+    println!("{output_string}");
 }
